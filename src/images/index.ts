3a4c74db85ceff368ec4eb31fbb231449b040e08
san.kim <san.kim@giantstepcorp.com>
2025-10-29 11:53:04 +0900

    이미지 변경

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,30 +1,72 @@
-import coverImage from "./cover.png"
-import image1 from "./image1.png"
-import image2 from "./image2.png"
-import image3 from "./image3.png"
-import image4 from "./image4.png"
-import image5 from "./image5.png"
-import image6 from "./image6.png"
-import image7 from "./image7.png"
-import image8 from "./image8.png"
-import image9 from "./image9.png"
-import image10 from "./image10.png"
-import image11 from "./image11.png"
-import image12 from "./image12.png"
+import coverImage from "./cover.jpeg"
+import image01 from "./01-BOS_0111완성.jpg"
+import image02 from "./02-BOS_0362완성.jpg"
+import image03 from "./03-BOS_0117완성.jpg"
+import image04 from "./04-BOS_0059완성.jpg"
+import image05 from "./05-BOS_0187완성.jpg"
+import image06 from "./06-BOS_0224완성.jpg"
+import image07 from "./07-BOS_0382완성.jpg"
+import image08 from "./08-BOS_0398완성.jpg"
+import image09 from "./09-BOS_0433완성.jpg"
+import image10 from "./10-BOS_0449완성.jpg"
+import image11 from "./11-BOS_0473완성.jpg"
+import image12 from "./12-BOS_0492완성.jpg"
+import image13 from "./13-BOS_0494완성.jpg"
+import image14 from "./14-BOS_0503완성.jpg"
+import image15 from "./15-BOS_0512완성.jpg"
+import image16 from "./16-BOS_0531완성.jpg"
+import image17 from "./17-BOS_0570완성.jpg"
+import image18 from "./18-BOS_0627완성.jpg"
+import image19 from "./19-BOS_0637완성.jpg"
+import image20 from "./20-BOS_0650완성.jpg"
+import image21 from "./21-BOS_0753완성.jpg"
+import image22 from "./22-BOS_0664완성.jpg"
+import image23 from "./23-BOS_0592완성.jpg"
+import image24 from "./24-BOS_0746완성.jpg"
+import image25 from "./25-BOS_0765완성.jpg"
+import image26 from "./26-BOS_0802완성.jpg"
+import image27 from "./27-BOS_0837완성.jpg"
+import image28 from "./28-BOS_0856완성.jpg"
+import image29 from "./29-BOS_0885완성.jpg"
+import image30 from "./30-BOS_0895완성.jpg"
+import image31 from "./31-BOS_0909완성.jpg"
+import image32 from "./32-BOS_0953완성.jpg"
+import image33 from "./33-BOS_0983완성.jpg"
 
 export const COVER_IMAGE = coverImage
 
 export const GALLERY_IMAGES = [
-  image1,
-  image2,
-  image3,
-  image4,
-  image5,
-  image6,
-  image7,
-  image8,
-  image9,
+  image01,
+  image02,
+  image03,
+  image04,
+  image05,
+  image06,
+  image07,
+  image08,
+  image09,
   image10,
   image11,
   image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+  image20,
+  image21,
+  image22,
+  image23,
+  image24,
+  image25,
+  image26,
+  image27,
+  image28,
+  image29,
+  image30,
+  image31,
+  image32,
+  image33,
 ]
